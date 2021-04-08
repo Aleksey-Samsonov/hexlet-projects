f222e0d93618a8e0a98a904ed44f8b50e4906e3a
Aleksey Samsonov <alexeysamsonov2@gmail.com>
2021-04-08 09:58:07 +0300

    add depend

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // "https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js"
 // const _ = require('lodash');
 
-import _ from 'lodash';
+import _ from "lodash";
 
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
+
+function name(params) {
+  const a = 14;
+}
